95834aae90670427410ac72ce0ce64dec7151496
jkutkut <jorgeregonzalez@gmail.com>
2021-04-14 20:42:42 +0200

    this class will handle the logic

---
+++ Classes/chessBoard.js
@@ -0,0 +1,12 @@
+class ChessBoard {
+    constructor(canvasWidth, canvasHeight) {
+        this._canvasSize = {w: canvasWidth, h: canvasHeight};
+
+        this._grid = new Array(8);
+        for(let i = 0; i < 8; i++){
+            grid[i] = new Array(8);
+        }
+
+        
+    }
+}
