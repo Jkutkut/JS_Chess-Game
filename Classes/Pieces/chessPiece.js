f4f5e82ffae065bf4b43c5243508c3bf327d0dc1
jkutkut <jorgeregonzalez@gmail.com>
2021-04-14 22:06:20 +0200

    pieces now show on screen but on the same position

--- Classes/Pieces/chessPiece.js
+++ Classes/Pieces/chessPiece.js
@@ -17,14 +17,16 @@ class ChessPiece {
     ];
 
     static ERRORS = {
+        INVALIDCLASS: new Error("The class used is not a valid class."),
         INVALIDPIECE: new Error("The piece must be a string with the name of the piece (rook, king...)."),
         INVALIDTEAM: new Error("The team selected is not valid. Use static values to selected it.")
     };
 
-    constructor(piece, team) {
+    constructor(team) {
+        let piece = this.constructor.name.toLowerCase();
         // Check correct input
         if (!ChessPiece.PIECENAME.includes(piece)) {
-            throw ChessPiece.ERRORS.INVALIDPIECE;
+            throw ChessPiece.ERRORS.INVALIDCLASS;
         }
         if (team != ChessPiece.TEAM.WHITE && team != ChessPiece.TEAM.BLACK) {
             throw ChessPiece.ERRORS.INVALIDTEAM;
@@ -44,8 +46,8 @@ class ChessPiece {
             }
         };
 
-        this._img = imgs[this.teamName + this.piece];
-        this.imgSize = [0, 0, 0, 0];
+        this._img = imgs[this.piece + this.teamName];
+        this.imgSize = [0, 0, 50, 50];
     }
 
     /**
@@ -72,5 +74,41 @@ class ChessPiece {
 
     get img() {
         return this._img;
+    }
+}
+
+class Bishop extends ChessPiece {
+    constructor(...arg) {
+        super(...arg);
+    }
+}
+
+class King extends ChessPiece {
+    constructor(...arg) {
+        super(...arg);
+    }
+}
+
+class Knight extends ChessPiece {
+    constructor(...arg) {
+        super(...arg);
+    }
+}
+
+class Pawn extends ChessPiece {
+    constructor(...arg) {
+        super(...arg);
+    }
+}
+
+class Queen extends ChessPiece {
+    constructor(...arg) {
+        super(...arg);
+    }
+}
+
+class Rook extends ChessPiece {
+    constructor(...arg) {
+        super(...arg);
     }
 }
--- Classes/chessBoard.js
+++ Classes/chessBoard.js
@@ -7,6 +7,15 @@ class ChessBoard {
         // color(0,0,100)
     ];
 
+    static PIECES = {
+        "bishop": Bishop,
+        "king": King,
+        "knight": Knight,
+        "pawn": Pawn,
+        "queen": Queen,
+        "rook": Rook
+    }
+
     constructor(canvasSize) {
         this._canvasSize = canvasSize;
         this._cellSize = canvasSize / 8;
@@ -15,8 +24,29 @@ class ChessBoard {
         for(let i = 0; i < 8; i++){
             this._grid[i] = new Array(8);
         }
-        
+        this._teamPieces = [new Set(), new Set()];
 
+        let order = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"];
+        // for(let i = 0; i < 8; i++){
+        //     grid[i][0].setPiece(new piece(order[i], 1));
+        //     grid[i][1].setPiece(new piece("pawn", 1));
+        //     grid[i][6].setPiece(new piece("pawn", -1));
+        //     grid[i][7].setPiece(new piece(order[i], -1));
+
+        //     //add pices to team var
+        //     team[0].push(grid[i][0].piece);
+        //     team[0].push(grid[i][1].piece);
+        //     team[1].push(grid[i][6].piece);
+        //     team[1].push(grid[i][7].piece);
+        // }
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 2; j++) {
+                let piece = new ChessBoard.PIECES["pawn"](1);
+                this._grid[i][j * 5 + 1] = piece;
+                this._teamPieces[j].add(piece);
+            }
+            // grid[i][6].setPiece(new piece("pawn", -1));
+        }
     }
 
     show() {
@@ -28,10 +58,20 @@ class ChessBoard {
                 rect(this.cellSize * i, this.cellSize * j, this.cellSize, this.cellSize);
             }
         }
+
+        for (let team of this._teamPieces) {
+            for (let piece of team) {
+                piece.show();
+            }
+        }
         pop();
     }
 
     get cellSize() {
         return this._cellSize;
+    }
+
+    get grid() {
+        return this._grid;
     }
 }
